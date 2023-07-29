fc306707e4d7a83b4008575c2cdd835bf5196398
vladsubbotin1 <vladislav.subbotin2010@gmail.com>
2023-07-29 16:06:08 +0400

    add: swap and Message API

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,19 @@
-import "@nomicfoundation/hardhat-toolbox";
-import { getHardhatConfigNetworks } from "@zetachain/networks";
-import "@zetachain/toolkit/tasks";
-import { HardhatUserConfig } from "hardhat/config";
+import './tasks/interact'
+import './tasks/deploy'
+import './tasks/swap'
+import './tasks/message'
+import '@nomicfoundation/hardhat-toolbox'
+import { getHardhatConfigNetworks } from '@zetachain/networks'
+import '@zetachain/toolkit/tasks'
+import { HardhatUserConfig } from 'hardhat/config'
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.7",
-  networks: {
-    ...getHardhatConfigNetworks(),
-  },
-};
+	solidity: {
+		compilers: [{ version: '0.6.6' /** For uniswap v2 */ }, { version: '0.8.7' }],
+	},
+	networks: {
+		...getHardhatConfigNetworks(),
+	},
+}
 
-export default config;
+export default config
